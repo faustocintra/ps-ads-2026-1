4c9fb730775b793c06ecb9c7d216f35b9a61db3e
marcus291005@gmail.com <130625110+marcusviniciusdesousa@users.noreply.github.com>
2026-02-26 15:09:19 +0000

    (26/02) Configuração da conexão e criação do primeiro controller

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,17 +2,34 @@ import express, { json, urlencoded } from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 
+
 import indexRouter from './routes/index.js'
 import usersRouter from './routes/users.js'
 
+
 const app = express()
+
 
 app.use(logger('dev'))
 app.use(json())
 app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
 
+
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+
+
+/**************** ROTAS *******************/
+
+
+// Middleware de verificação de autorização
+import authMiddleware from './middleware/auth.js'
+app.use(authMiddleware)
+
+
+import customersRoute from './routes/customers.js'
+app.use('/customers', customersRoute)
+
 
 export default app
