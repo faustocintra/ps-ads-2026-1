a33d0aea8dfcfba79bd937de96f4cb17126ff633
Eduarda818 <marveeduarda9@gmail.com>
2026-03-05 15:18:38 +0000

    (26/02) Configuração da conexão e criação do primeiro controller

--- back-end/src/routes/customers.js
+++ back-end/src/routes/customers.js
@@ -2,8 +2,11 @@ import { Router } from 'express'
 import controller from '../controllers/customers.js'
 const router = Router()
 
-
+// onde for POST, vai no contoller e cria
+//POST precisa ter corpo na requisição pois ele precisa mandar dados
 router.post('/', controller.create)
+
+router.get('/', controller.retrieveAll)
 
 
 export default router
