adbff1f51441ec73ba3e5937b67551ca9c3862ab
marcus291005@gmail.com <130625110+marcusviniciusdesousa@users.noreply.github.com>
2026-04-09 13:21:38 +0000

    (09/04) Criação do model Seller e seu controller e route, assim como a criação de um vendedor

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -43,4 +43,7 @@ app.use('/cars', carsRoute)
 import usersRoute from './routes/users.js'
 app.use('/users', usersRoute)
 
+import sellersRoute from './routes/sellers.js'
+app.use('/sellers', sellersRoute)
+
 export default app
