441d82948bde8e3c3c2efd49a18039268895675e
marcus291005@gmail.com <130625110+marcusviniciusdesousa@users.noreply.github.com>
2026-04-23 13:56:17 +0000

    (23/04) Início da implementação de autenticação no front-end

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -5,9 +5,14 @@ import Homepage from '../pages/Homepage'
 import CarsList from '../pages/cars/CarsList'
 import CustomersList from '../pages/customers/CustomersList'
 
+import LoginPage from '../pages/LoginPage'
+
+
 export default function AppRoutes() {
  return <Routes>
    <Route path="/" element={ <Homepage /> } />
+
+   <Route path="/login" element={ <LoginPage /> } />
 
 
    <Route path="/cars" element={ <CarsList /> } />
