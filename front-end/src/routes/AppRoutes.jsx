121c9083df7116038be0ff4c87a8bf4a87863b73
Hugo Mendes <hugogs.mendes@gmail.com>
2026-04-23 14:02:06 +0000

    (23/04) Início da implementação de autenticação no front-end

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -5,13 +5,19 @@ import Homepage from '../pages/Homepage'
 import CarsList from '../pages/cars/CarsList'
 import CustomersList from '../pages/customers/CustomersList'
 
+import LoginPage from '../pages/LoginPage'
+
 export default function AppRoutes() {
- return <Routes>
-   <Route path="/" element={ <Homepage /> } />
+  return <Routes>
+    <Route path="/" element={ <Homepage /> } />
 
 
-   <Route path="/cars" element={ <CarsList /> } />
-   <Route path="/customers" element={ <CustomersList /> } />
+    <Route path="/login" element={ <LoginPage /> } />
 
- </Routes>
+
+    <Route path="/cars" element={ <CarsList /> } />
+    <Route path="/customers" element={ <CustomersList /> } />
+
+
+  </Routes>
 }
