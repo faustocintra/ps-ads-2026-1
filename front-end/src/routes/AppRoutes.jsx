53d28045b4644c7a7c0ca2a158e67e3f6330be31
Andrea B. Lima <andreab-lima@hotmail.com>
2026-04-23 16:17:11 +0000

    (23/04) Início da implementação de autenticação no front-end

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,15 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
-
 import Homepage from '../pages/Homepage'
-
 import CarsList from '../pages/cars/CarsList'
 import CustomersList from '../pages/customers/CustomersList'
+import LoginPage from '../pages/LoginPage'
+
 
 export default function AppRoutes() {
  return <Routes>
    <Route path="/" element={ <Homepage /> } />
-
-
+   <Route path="/login" element={ <LoginPage /> } />
    <Route path="/cars" element={ <CarsList /> } />
    <Route path="/customers" element={ <CustomersList /> } />
 
