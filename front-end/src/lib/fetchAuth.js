82f511f75edf33cbe457e855abfc5121dd70052f
marcus291005@gmail.com <130625110+marcusviniciusdesousa@users.noreply.github.com>
2026-05-07 15:02:21 +0000

    (30/04) Término do fluxo de autenticação no front-end

--- front-end/src/lib/fetchAuth.js
+++ front-end/src/lib/fetchAuth.js
@@ -22,7 +22,7 @@ function buildOptions(method = 'GET', body = null) {
      'Content-Type': 'application/json; charset=UTF-8'
    },
    // Pede para o back-end enviar cookies
-   // credentials: 'include'
+   credentials: 'include'
  }
 
 
@@ -32,13 +32,13 @@ function buildOptions(method = 'GET', body = null) {
 
  // Verifica se existe um token gravado no localStorage
  // e o inclui nos headers, se for o caso
- const token = window.localStorage.getItem(import.meta.env.VITE_AUTH_TOKEN_NAME)
+ //const token = window.localStorage.getItem(import.meta.env.VITE_AUTH_TOKEN_NAME)
 
 
  // console.log({token})
 
 
- if(token) options.headers.authorization = `Bearer ${token}`
+ // if(token) options.headers.authorization = `Bearer ${token}`
 
 
  return options
--- front-end/src/ui/MainMenu.jsx
+++ front-end/src/ui/MainMenu.jsx
@@ -69,10 +69,28 @@ export default function MainMenu() {
           onClick={handleClose}
           component={Link}
           to="/customers"
+          divider
         >
 
           Listagem de clientes
         </MenuItem>        
+        
+         <MenuItem
+        onClick={handleClose}
+        component={Link}
+        to="/users"
+      >
+        Listagem de usuários
+      </MenuItem>
+
+
+      <MenuItem
+        onClick={handleClose}
+        component={Link}
+        to="/users/new"
+      >
+        Cadastro de usuários
+      </MenuItem>
         
       </Menu>
     </div>
